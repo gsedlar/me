025e43bde40b6fdc657bb15926809b30ac02e7cb
gsedlar <156832670+gsedlar@users.noreply.github.com>
2025-01-26 22:42:00 -0500

    Create main.py

---
+++ HW1/main.py
@@ -0,0 +1,23 @@
+Data
+  -> poke.csv
+  -> locations.csv
+Q1
+  -> Q1.py
+Q2 
+  -> Q2.py
+Q3
+  -> Q3.py
+Q4
+  -> Q4.py
+Q5 
+  -> Q5.py
+Q6
+  -> Q6.py
+Q7
+  -> Q7.py
+Q8 
+  -> Q8.py
+Q9
+  -> Q9.py
+Q10
+  -> Q10.py
